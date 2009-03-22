4d9fe3805185d996a95ff340cc0c0b970dd32226
Matijs van Zuijlen <matijs@matijs.net>
2009-03-22 21:18:31 +0100

    Test parse of version Expand version rule

--- test/basics_test.rb
+++ test/basics_test.rb
@@ -5,6 +5,9 @@ class BasicsTest < Test::Unit::TestCase
     setup do
       @parser = VB6Parser.new
     end
+    should "parse version" do
+      assert_equal [], @parser.parse("VERSION 5.00")
+    end
     should "parse empty string" do
       assert_equal [], @parser.parse(" ")
     end
